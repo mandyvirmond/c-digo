e2b9d8c226ec895af9ab2837e3d868ccda3bd9f0
mandyvirmond <virmond.amanda@escola.pr.gov.br>
2025-06-03 10:47:07 +0000

    código

---
+++ main.js
@@ -0,0 +1,69 @@
+const carro = {
+    marca: "Toyota",
+    cor: "preto",
+    ano: 2020
+};
+console.log(carro.marca);
+
+const cadeira = {
+    material: "madeira",
+    cor: "vermelha",
+    temEncosto: true
+};
+console.log(cadeira.material);
+
+const telefone = {
+    modelo: "iPhone 13",
+    temCamera: true,
+    armazenamento: "128GB"
+};
+console.log(telefone.modelo);
+
+const relogio = {
+    tipo: "digital",
+    marca: "Casio",
+    resistenteAAgua: true
+};
+console.log(relogio.marca);
+
+const fogao = {
+    bocas: 4,
+    temForno: true,
+    cor: "branco"
+};
+console.log(fogao.bocas);
+
+const ventilador = {
+    potencia: "60W",
+    velocidades: 3,
+    cor: "azul"
+};
+console.log(ventilador.potencia);
+
+const mochila = {
+    compartimentos: 3,
+    cor: "preta",
+    temAlmofadaCostas: true
+};
+console.log(mochila.compactimentos);
+
+const copo = {
+    material: "vidro",
+    capacidadeML: 300,
+    eDescartavel: false
+};
+console.log(copo.capacidadeML);
+
+const chave = {
+    tipo: "residencial",
+    material: "metal",
+    possuiEtiqueta: true
+};
+console.log(chave.tipo);
+
+const bicicleta = {
+    marchas: 21,
+    aro: 29,
+    cor: "verde"
+};
+console.log(bicicleta.aro);
